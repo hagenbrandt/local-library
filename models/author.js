46f9df495c02f80649f6a885d2c0d8c2a464f3b7
hagenbrandt <59567907+hagenbrandt@users.noreply.github.com>
2020-02-23 15:48:25 +0100

    Make corrections

--- models/author.js
+++ models/author.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const moment = require('moment')
+const moment = require('moment') // For date handling.
 
 const Schema = mongoose.Schema
 
@@ -10,28 +10,44 @@ const AuthorSchema = new Schema({
   date_of_death: { type: Date },
 })
 
-// Virtual for author's full name
+// Virtual for author "full" name.
 AuthorSchema.virtual('name').get(function() {
-  const fullname = ''
+  let fullname = ''
+
   if (this.first_name && this.family_name) {
     fullname = this.family_name + ', ' + this.first_name
   }
-  if (!this.first_name || !this.family_name) {
+
+  if (!this.first_name && !this.family_name) {
     fullname = ''
   }
   return fullname
 })
 
-// Virtual for author's lifespan
-AuthorSchema.virtual('lifespan').get(function() {
-  return (
-    this.date_of_death.getYear() - this.date_of_birth.getYear()
-  ).toString()
-})
-
-// Virtual for author's URL
+// Virtual for this author instance URL.
 AuthorSchema.virtual('url').get(function() {
   return '/catalog/author/' + this._id
 })
 
+AuthorSchema.virtual('lifespan').get(function() {
+  let lifetime_string = ''
+  if (this.date_of_birth) {
+    lifetime_string = moment(this.date_of_birth).format('MMMM Do, YYYY')
+  }
+  lifetime_string += ' - '
+  if (this.date_of_death) {
+    lifetime_string += moment(this.date_of_death).format('MMMM Do, YYYY')
+  }
+  return lifetime_string
+})
+
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function() {
+  return moment(this.date_of_birth).format('YYYY-MM-DD')
+})
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function() {
+  return moment(this.date_of_death).format('YYYY-MM-DD')
+})
+
+// Export model.
 module.exports = mongoose.model('Author', AuthorSchema)
